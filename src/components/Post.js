3b48efe3355de0074538fe84c609b39f53bb2af3
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-01 12:44:46 +0100

    Removed refferences to country flags and getting ready to add dessert page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client";
+
 export default function Post() {
   const [postData, setPost] = useState(null);
-
   useEffect(() => {
     sanityClient
       .fetch(
